4bf823cd81c78f5aaa4dd9e874a75ed0ea23ae06
Thays Ribeiro <thaysribeiros@gmail.com>
2021-07-27 15:43:15 -0700

    Create my_string_formatting.rb
    Created a my_string_formatting function which takes 3 parameters (firstname, lastname and age) and prints a string composed value.

---
+++ Quest03/my_string_formatting.rb
@@ -0,0 +1,16 @@
+##
+##
+## QWASAR.IO -- my_string_formatting
+##
+##
+## @param {String} param_1
+## @param {String} param_2
+## @param {Integer} param_3
+##
+##
+
+def my_string_formatting(first_name, last_name, age)
+    puts "Hello, my name is #{first_name} #{last_name}, I'm #{age}."
+end
+
+#my_string_formatting("Baby", "Yoda", "50")
