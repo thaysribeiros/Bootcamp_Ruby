1e8dbd085e2936bee46ab8ecabf5649a8c49ee0b
Thays Ribeiro <thaysribeiros@gmail.com>
2021-07-27 15:51:58 -0700

    Create my_each.rb
    Create a function my_each which receives an array of strings as parameter and iterate over the array and use a function which print for each value.
    This function return nothing.

---
+++ Quest05/my_each.rb
@@ -0,0 +1,19 @@
+##
+##
+## QWASAR.IO -- my_each
+##
+##
+## @param {String[]} param_1
+##
+##
+
+def my_each(array_string)
+    i = 0
+    while (i != array_string.length)
+        puts array_string[i]
+        i += 1
+    end
+end
+
+#array_string = ["blah1", "blah2", "blah3"]
+#my_each(array_string)
