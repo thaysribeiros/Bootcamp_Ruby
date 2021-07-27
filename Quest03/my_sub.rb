6268180660f555e08b97c19a452ef308f3ed5afb
Thays Ribeiro <thaysribeiros@gmail.com>
2021-07-27 15:41:12 -0700

    Create my_sub.rb
    Created a my_sub function which takes 2 parameters (nbr1 and nbr2) and returns a value.
    This value is the result of the subtraction of nbr1 and nbr2 parameters.

---
+++ Quest03/my_sub.rb
@@ -0,0 +1,14 @@
+##
+##
+## QWASAR.IO -- my_sub
+##
+##
+## @param {Integer} param_1
+## @param {Integer} param_2
+##
+## @return {integer}
+##
+
+def my_sub(nbr1, nbr2)
+    return nbr1 - nbr2
+end
