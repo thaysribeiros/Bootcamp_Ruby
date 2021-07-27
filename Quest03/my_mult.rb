56afa61cf6a577486190049b20b830d268dbf3f8
Thays Ribeiro <thaysribeiros@gmail.com>
2021-07-27 15:42:02 -0700

    Create my_mult.rb
    Created a my_mult function which takes 2 parameters (nbr1 and nbr2) and returns a value.
    This value is the result of the multiplication of nbr1 and nbr2 parameters.

---
+++ Quest03/my_mult.rb
@@ -0,0 +1,14 @@
+##
+##
+## QWASAR.IO -- my_mult
+##
+##
+## @param {Integer} param_1
+## @param {Integer} param_2
+##
+## @return {integer}
+##
+
+def my_mult(nbr1, nbr2)
+    return nbr1*nbr2
+end
