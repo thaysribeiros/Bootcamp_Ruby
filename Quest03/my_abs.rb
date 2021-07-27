dac162741c4de34ff6df2012e6c53ee64f1c0b28
Thays Ribeiro <thaysribeiros@gmail.com>
2021-07-27 15:39:19 -0700

    Create my_abs.rb
    Reproduce behavior of an abs() function. It returns always the positive value of a number.

---
+++ Quest03/my_abs.rb
@@ -0,0 +1,20 @@
+##
+##
+## QWASAR.IO -- my_abs
+##
+##
+## @param {Integer} param_1
+##
+## @return {integer}
+##
+
+def my_abs(n)
+    if (n < 0)
+        return n *= -1
+    else
+        return n
+    end
+end
+
+#my_abs(-100)
+#my_abs(42)
